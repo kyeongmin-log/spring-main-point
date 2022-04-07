1aa0e2c9823ac424567c3fdddf2900356df2b396
kyeongmin-log <dotcom7860@gmail.com>
2022-04-07 18:15:43 +0900

    Add 다양한 설정 형식 지원 - Java Code, Xml

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,19 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class XmlAppContext {
+
+    // xml로 bean을 설정한다. 작성 형식이 class -> xml로 바뀐 것 뿐 사용하는 방법은 동일하다.
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
